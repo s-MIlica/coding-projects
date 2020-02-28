56ecb62ff2dd83db90664b4eab79438d9ad0bef7
Milica Stepanović <stepa.milica@gmail.com>
2020-02-28 17:31:03 +0100

    Updates

---
+++ precache-manifest.4e2a5a4b86104836263a5d5b7e02e56a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52ba80dd8f7c265058377eff9a46a428",
+    "url": "/coffee-timer/index.html"
+  },
+  {
+    "revision": "499da6132a7d29db7ed7",
+    "url": "/coffee-timer/static/css/main.146d4b8c.chunk.css"
+  },
+  {
+    "revision": "6ac5aac57af3051fd655",
+    "url": "/coffee-timer/static/js/2.c4a3f806.chunk.js"
+  },
+  {
+    "revision": "b08610a15aea65eb1a2f6008ea11a714",
+    "url": "/coffee-timer/static/js/2.c4a3f806.chunk.js.LICENSE"
+  },
+  {
+    "revision": "499da6132a7d29db7ed7",
+    "url": "/coffee-timer/static/js/main.71649bc3.chunk.js"
+  },
+  {
+    "revision": "32b2b2798476dfae389e",
+    "url": "/coffee-timer/static/js/runtime-main.b00f054e.js"
+  },
+  {
+    "revision": "76af347e09c08dac4bf6dde85aa3c42a",
+    "url": "/coffee-timer/static/media/autotune.76af347e.mp3"
+  }
+]);
